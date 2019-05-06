877d71d3d871bfbe24f313d7424f21f61736748f
Geoffery Powell <powellgeoffery@gmail.com>
2019-05-05 19:29:48 -0500

    Started Main Menu
    - Added Sound
    - Added Settings Button
    - Added Fullscreen

--- app/src/main/java/com/thecoredepository/tangent/MainActivity.java
+++ app/src/main/java/com/thecoredepository/tangent/MainActivity.java
@@ -21,4 +21,80 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main_menu);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+
+    //=====================================================================
+    //                               MUSIC
+    //=====================================================================
+
+    private void stopPlaying()
+    {
+        if (soundPlayer != null)
+        {
+            soundPlayer.reset();
+            soundPlayer.stop();
+            soundPlayer.release();
+            soundPlayer=null;
+        }
+    }
+
+    private void startPlaying(@RawRes int sound, boolean loopingTrue)
+    {
+        Log.i("My App", "STOP");
+        stopPlaying();
+        soundPlayer = MediaPlayer.create(getApplicationContext(), sound);
+        soundPlayer.start();
+        soundPlayer.setLooping(loopingTrue);
+        Log.i("My App", "START");
+    }
+
+    //=====================================================================
+    //                            MUSIC END
+    //=====================================================================
+
+    //=====================================================================
+    //                            FULLSCREEN
+    //=====================================================================
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+            startPlaying(R.raw.cyoa01smallfinal, true);
+        }
+        else
+        {
+            stopPlaying();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    //=====================================================================
+    //                          FULLSCREEN END
+    //=====================================================================
 }
