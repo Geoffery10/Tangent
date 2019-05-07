fe707d5441d41a8e41423b6bfb8fc974a60231cb
Geoffery Powell <powellgeoffery@gmail.com>
2019-05-06 22:41:06 -0500

    Added Story Data Structure

--- app/src/main/java/com/thecoredepository/tangent/story/GenerateStory.java
+++ app/src/main/java/com/thecoredepository/tangent/story/GenerateStory.java
@@ -10,16 +10,46 @@ public class GenerateStory
 
     public void generateStory()
     {
+        String key;
+        int background = 0;
+        int forground = 0;
+        String charName;
         String speechText;
+        Boolean continueTrue;
+        String optionA;
+        String optionB;
+        String leftChild;
+        String rightChild;
+
         //int nodesInStory = 3;
 
         //=====================================================================
         //                            STORY DATA
         //=====================================================================
 
-        speechText = "Hello, friendly traveler!";
-        StoryObject story1_1;
-        storyArray.add(story1_1 = new StoryObject("1_1", 0, 0, "Death", speechText, true, null, null, "2_1", "2_2"));
+        //Formating
+        //key is were it is in the tree
+        //background is the background index
+        //forground is the forground index
+        //charName id the talking character's Name
+        //speechText is the Dialog of said character
+        //continueTrue is true when the only option is continue else it's false
+        //option A and B = "" unless continueTrue is false
+        //leftChild equals the next key which is either continue or optionA
+        //rightChild equals "" unless there us an option B
+
+        key = "0_1";
+        background = 0;
+        forground = 0;
+        charName = "Steven";
+        speechText = "OH NO! I’m late again! I should call and explai…";
+        continueTrue = true;
+        optionA = "";
+        optionB = "";
+        leftChild = "1_1";
+        rightChild = "";
+        StoryObject story0_1;
+        storyArray.add(story0_1 = new StoryObject(key, background, forground, charName, speechText, continueTrue, optionA, optionB, leftChild, rightChild));
 
         //=====================================================================
         //                         STORY DATA END
@@ -29,7 +59,7 @@ public class GenerateStory
     public StoryObject getStoryByID(String key)
     {
         int foundAt = 0;
-
+        
         return storyArray.get(foundAt);
     }
 }
